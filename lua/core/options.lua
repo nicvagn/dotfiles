82b77decef7f231d3d2607a66f8ddb094e11bc1d
Nicolas Vaagen <nicolasvaagen@gmail.com>
2024-03-31 15:37:24 -0500

    removed relative numbers

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,14 +1,12 @@
 local set = vim.opt
 
 set.number = true -- line number
-set.relativenumber = true
-set.mouse = "a" -- Enable mouse
 set.tabstop = 4
 set.shiftwidth = 4
 set.scrolloff = 8 -- Keep minimum x number of screen lines above and below the cursor
 set.clipboard = "unnamedplus"
-set.swapfile = false
-set.hlsearch = false -- Highlight search results
+set.swapfile = true
+set.hlsearch = true  -- Highlight search results
 set.incsearch = true -- As you type, match the currently typed word w/o pressing enter
 set.cursorline = true
 set.termguicolors = true
