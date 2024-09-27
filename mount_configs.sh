04e88e2328e1495b653312fb72b692152c257412
Nicolas Vaagen <nicolasvaagen@gmail.com>
2024-09-26 21:59:37 -0600

    cleanup

--- mount_configs.sh
+++ mount_configs.sh
@@ -2,8 +2,11 @@
 
 # SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 cd $SCRIPT_DIR
-echo "monting config's w mout --bind"
+echo "mounting config's w mount --bind"
 mount --bind /home/nrv/.config/ /home/nrv/git/dotfiles/dotconfig/
 mount --bind /home/nrv/.config/nvim/ /home/nrv/git/dotfiles/nvim/
-mount --bind /home/nrv/.oh-my-bash/ /home/nrv/git/dotfiles/oh-my-bash/
 mount --bind /home/nrv/.oh-my-zsh/ /home/nrv/git/dotfiles/oh-my-zsh/
+mount --bind /home/nrv/.vimrc /home/nrv/git/dotfiles/vimrc
+mount --bind /home/nrv/.zshrc /home/nrv/git/dotfiles/zshrc
+mount --bind /home/nrv/.zprofile /home/nrv/git/dotfiles/zprofile
+mount --bind /home/nrv/.gitconfig /home/nrv/git/dotfiles/gitcontig
