c0d66b484f479d4341a497d5d7f6795757b4d85f
Nicolas Vaagen <37600758+nicvagn@users.noreply.github.com>
2024-09-29 13:39:13 -0600

    update

---
+++ dotconfig/.config/cli-chess/custom_style.py
@@ -0,0 +1,8 @@
+# This file is used to override the default style of cli-chess. It must be kept in dictionary format.
+# Colors are expected to be HTML color names (e.g. seagreen) or HTML hex colors (e.g. #2E8B57)
+# Restarting cli-chess or pressing [CTRL+R] on any screen will force a style refresh.
+# Visit the cli-chess github page (https://github.com/trevorbayless/cli-chess/) for more styling information.
+
+{
+
+}
