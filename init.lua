8ebaead5e61d5ce295c784091070716f01c0deca
Nicolas Vaagen <37600758+nicvagn@users.noreply.github.com>
2024-09-28 21:58:38 -0600

    lsp config: it is a mess
    uncovered how much of a mess my lsp config is

--- init.lua
+++ init.lua
@@ -10,6 +10,11 @@ vim.g.python3_host_prog = "/usr/bin/python3"
 -- little longer <leader> time
 vim.g.timeoutlen = 1800
 
+-- utils
+require("utils.cspell") -- spelling
+require("utils.path") -- path utils like getting rot dir of a git repo
+
+-- core
 require("core.colourscheme")
 require("core.options")
 require("core.keymaps")
@@ -19,8 +24,8 @@ require("core.lualine-config")
 require("core.normalcmds")
 require("core.cmp-src") -- dictionary
 
-require("utils.cspell") -- spelling
-require("utils.path") -- path utils like getting rot dir of a git repo
+-- TODO: MAKE LSP CONFIG MORE GOOD
+-- require("core.lsp") broken, I forgot to include and did my own lsp's in the bottom of this file
 -- language servers
 local lsp_conf = require("lspconfig")
 
@@ -42,4 +47,4 @@ lsp_conf.cmake.setup({}) -- cmake
 
 lsp_conf.html.setup({}) -- html
 
-lsp_conf.tsserver.setup({}) -- type script
+lsp_conf.ts_ls.setup({}) -- type script
