c377e4d8d63f1f7ee39faa5b37de9b8a66b4852b
Nicolas Vaagen <nicolasvaagen@gmail.com>
2024-10-05 10:18:00 -0600

    vscode inter and keymaps

--- init.lua
+++ init.lua
@@ -2,21 +2,27 @@
 L_DVORAK = true
 
 -- AI auto complete
-vim.g.ai_auto = true
+vim.g.ai_auto = false
 -- providers
 vim.g.python3_host_prog = "/usr/bin/python3"
 
--- utils
-require("utils.cspell") -- spelling
-require("utils.path") -- path utils like getting rot dir of a git repo
+if vim.g.VSCode then
+	-- VSCode extension
+	require("core.keymaps")
+else
+	-- ordinary Neovim
+	-- utils
+	require("utils.cspell") -- spelling
+	require("utils.path") -- path utils like getting rot dir of a git repo
 
--- core
-require("core.colourscheme")
-require("core.options")
-require("core.keymaps")
-require("core.autocmd")
-require("core.lazy")
-require("core.lualine-config")
-require("core.normalcmds")
-require("core.cmp-src") -- dictionary
-require("core.lsp") -- lsp's
+	-- core
+	require("core.colourscheme")
+	require("core.options")
+	require("core.keymaps")
+	require("core.autocmd")
+	require("core.lazy")
+	require("core.lualine-config")
+	require("core.normalcmds")
+	require("core.cmp-src") -- dictionary
+	require("core.lsp") -- lsp's
+end
