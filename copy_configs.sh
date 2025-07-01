5aba9b01e62d2dfb7fb043b8010bccf828601a98
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-07-01 15:05:15 -0600

    updated copyconfigs to coppy dconf stuff

--- copy_configs.sh
+++ copy_configs.sh
@@ -5,13 +5,15 @@ cd $SCRIPT_DIR
 echo $SCRIPT_DIR
 cp -rf /home/nrv/.oh-my-zsh/ /home/nrv/git/dotfiles/oh-my-zsh/
 
-cp -f /home/nrv/.vimrc /home/nrv/git/dotfiles/vimrc
-cp -f /home/nrv/.zshrc /home/nrv/git/dotfiles/zshrc
-cp -f /home/nrv/.gitconfig /home/nrv/git/dotfiles/gitcontig
+cp -f /home/nrv/.vimrc ./vimrc
+cp -f /home/nrv/.zshrc ./zshrc
+cp -f /home/nrv/.gitconfig ./gitcontig
 
-cp -f /home/nrv/.config/background   /home/nrv/git/dotfiles/background
+cp -f /home/nrv/.config/background  ./background
 
-cp -f /home/nrv/.config/monitors.xml /home/nrv/git/dotfiles/monitors.xml
-cp -fr /home/nrv/.config/environment.d/ /home/nrv/git/dotfiles/
+cp -f /home/nrv/.config/monitors.xml ./monitors.xml
+cp -fr /home/nrv/.config/environment.d/ ./
 
-cp -fr /home/nrv/.config/enchant /home/nrv/git/dotfiles/dotconfig_enchant
+cp -fr /home/nrv/.config/enchant ./dotconfig_enchant
+
+dconf dump / > ./dconf_dump
